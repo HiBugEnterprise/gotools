1e132b4da6c7f19fe41c2a672d1a680a75322d4b
HiBug <82739039+HiBugEnterprise@users.noreply.github.com>
2023-12-05 11:12:48 +0800

    feat: 增加获取ip

--- ips/iptools.go
+++ ips/iptools.go
@@ -1,12 +1,50 @@
 package ips
 
 import (
+	"errors"
 	"fmt"
 	"github.com/PandaXGO/PandaKit/httpclient"
+
+	"net"
+	"net/http"
+	"strings"
 )
 
 const ipurl = "https://ip.cn/api/index"
 const UNKNOWN = "XX XX"
+
+// GetIP returns request client ip.
+func GetIP(r *http.Request) (string, error) {
+	ip := r.Header.Get("Remote-Host")
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+
+	ip = r.Header.Get("X-Forwarded-For")
+	for _, i := range strings.Split(ip, ",") {
+		if net.ParseIP(i) != nil {
+			return i, nil
+
+		}
+
+	}
+
+	ip = r.Header.Get("X-Real-IP")
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+
+	return "", errors.New("no valid ip found")
+}
 
 func GetRealAddressByIP(ip string) string {
 	if ip == "127.0.0.1" || ip == "localhost" {
